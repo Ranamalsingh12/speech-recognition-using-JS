6c9c001cef607369b30120eb085f01e4efbea3d5
ranamalsingh12 <ranams99911@gmail.com>
2021-11-29 22:11:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// window.SpeechRecognition = window.SpeechRecognition ||window.webkitSpeechRecogintion;
+
+// const recognition = new SpeechRecognition();
+// recognition.interimResults = true;
+
+// let p = document.createElement('p');
+// const words = document.querySelector('.words');
+// words.appendChild(p);
+
+// recognition.addEventListner('result', e=>{
+//     console.log(e);
+//     const transcript = Array.from(e.results)
+//     .map(result => result[0])
+//     .map(result => result.transcript)
+//     .join('')
+
+//     p.textContent = transcript;
+//     if (e.results[0].isFinal) {
+//         p = document.createElement('p');
+//         words.appendChild(p);
+//       }
+//     console.log(transcript);
+// });
+
+// recognition.addEventListner('end',recognition.start);
+// recognition.start();
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+    p.textContent = poopScript;
+
+    if (e.results[0].isFinal) {
+      p = document.createElement('p');
+      words.appendChild(p);
+    }
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
+
